40a3eaa609905d8dda96f52d90a22fcfa4fe01b9
Toni Rachev <tonirachev92@gmail.com>
2024-06-05 23:15:26 +0300

    updated

--- src/components/book-list/BookList.jsx
+++ src/components/book-list/BookList.jsx
@@ -1,7 +1,14 @@
-export default function BookList({children}) {
+import BookCard from "../book-card/BookCard.jsx";
+
+import style from "./BookList.module.css";
+
+export default function BookList({books}) {
+
     return (
-        <section>
-            {children}
+        <section className={style.container}>
+            {books.data.products.slice(0, 5).map(book =>
+                <BookCard key={book._id} book={book}/>
+            )}
         </section>
     )
 }
