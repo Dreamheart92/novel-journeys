71ce8dfd370a644f5dc3a8f181f28254b2e1fd93
Toni Rachev <tonirachev92@gmail.com>
2024-06-05 23:17:11 +0300

    added basic fetching

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,21 @@
 import Hero from "../../components/hero/Hero.jsx";
 
 import style from "./Home.module.css";
+import {useHttp} from "../../hooks/useHttp.js";
+
+import {getRecentBooks} from "../../api/books.js";
+import BookList from "../../components/book-list/BookList.jsx";
 
 export default function Home() {
+    const {data: recentlyAddedBooks, isLoading, error} = useHttp({url: getRecentBooks, defaultValue: []});
+
     return (
         <section className={style.container}>
             <Hero/>
+
+            {isLoading === false &&
+                <BookList books={recentlyAddedBooks}/>
+            }
         </section>
     )
 }
