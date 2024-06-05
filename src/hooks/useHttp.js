d7b96dd5538661fc121bb873495d8beb6331b085
Toni Rachev <tonirachev92@gmail.com>
2024-06-05 23:16:08 +0300

    setting is loading state before and after request

--- src/hooks/useHttp.js
+++ src/hooks/useHttp.js
@@ -4,16 +4,28 @@ const sendRequest = (url, settings) => {
     return fetch(url, settings);
 }
 
-export const useHttp = (url, settings, defaultValue) => {
+export const useHttp = ({url, settings, defaultValue}) => {
     const [data, setData] = useState(defaultValue);
     const [isLoading, setIsLoading] = useState(null);
     const [error, setError] = useState(null);
 
 
     useEffect(() => {
+        setIsLoading(true);
         fetch(url, settings)
             .then((response) => response.json())
-            .then((data) => console.log(data))
-            .catch((error) => console.log(error));
+            .then((data) => {
+                setData(data);
+            })
+            .catch((error) => console.log(error))
+            .finally(() => {
+                setIsLoading(false);
+            })
     }, [url]);
+
+    return {
+        data,
+        isLoading,
+        error
+    }
 }
