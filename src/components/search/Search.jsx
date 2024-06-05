b070ff5ba1fe7729398a27f7df6806cb078deaf4
Toni Rachev <tonirachev92@gmail.com>
2024-06-05 19:58:37 +0300

    added container for the search + search icon

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,7 +1,16 @@
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons/faMagnifyingGlass";
+
 import style from "./Search.module.css";
 
 export default function Search() {
     return (
-        <input className={style.search} type="text" placeholder="Search books..."/>
+        <div className={style.container}>
+            <input className={style.search} type="text" placeholder="Search books..."/>
+
+            <div className={style['search-icon']}>
+                <FontAwesomeIcon icon={faMagnifyingGlass}/>
+            </div>
+        </div>
     )
 }
