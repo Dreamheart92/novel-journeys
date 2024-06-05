23f3b3ee3f6b866e7eb67df091f7106755c7a3cc
Toni Rachev <tonirachev92@gmail.com>
2024-06-05 18:36:50 +0300

    Added basic template

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,30 @@
+export default function Header() {
+    return (
+        <section>
+            <div>
+                <div>
+                    <h1>Logo</h1>
+                </div>
+
+                <div>
+                    <input type="text" placeholder="Search books..."/>
+                </div>
+
+                <div>
+                    <button>Sign in</button>
+                </div>
+
+            </div>
+
+            <div>
+                <ul>
+                    <li>Menu item</li>
+                    <li>Menu item 2</li>
+                    <li>Menu item 3</li>
+                    <li>Menu item 4</li>
+                    <li>Menu item 5</li>
+                </ul>
+            </div>
+        </section>
+    )
+}
