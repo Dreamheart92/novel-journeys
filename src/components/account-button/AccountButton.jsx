842ad8c6a570f14dbfe4615ed9061eb3702f9b24
Toni Rachev <tonirachev92@gmail.com>
2024-06-06 16:57:29 +0300

    added link to login page

--- src/components/account-button/AccountButton.jsx
+++ src/components/account-button/AccountButton.jsx
@@ -1,7 +1,12 @@
 import style from "./AccountButton.module.css";
+import {Link} from "react-router-dom";
 
 export default function AccountButton() {
     return (
-        <button className={style.account}>Sign In</button>
+        <button className={style.account}>
+            <Link to="/login">
+                Sign In
+            </Link>
+        </button>
     )
 }
