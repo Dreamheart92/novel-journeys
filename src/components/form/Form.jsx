eb3eb3528a6fdfd4b310f822f90721078b5c5831
Toni Rachev <tonirachev92@gmail.com>
2024-06-06 22:56:55 +0300

    Update Form.jsx

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,7 +1,15 @@
-export default function Form({formData, children}) {
+import {useEffect} from "react";
+
+export default function Form({formData, children, ...props}) {
+    useEffect(() => {
+        if (formData) {
+            console.log("form data");
+        }
+    }, [formData]);
+
     return (
-        <>
+        <form {...props}>
             {children}
-        </>
+        </form>
     )
 }
