3189da95976dfcf8246b096107fa1083e5f46b77
Toni Rachev <tonirachev92@gmail.com>
2024-06-06 19:39:09 +0300

    displaying basic error

--- src/components/shared/input/Input.jsx
+++ src/components/shared/input/Input.jsx
@@ -1,15 +1,18 @@
 import style from "./Input.module.css";
 
 export default function Input({label, type = "text", ...props}) {
-    console.log(props.state.error);
+    const value = props.state.value;
+    const error = props.state.error;
+
+    const showError = error && props.state.isDirty;
 
     return (
         <div className={style.container}>
             <label htmlFor={label}>{label[0].toUpperCase() + label.slice(1)}</label>
-            <input {...props.handlers} type={type} name={label}/>
+            <input {...props.handlers} type={type} name={label} value={value}/>
 
-            {props.state.error &&
-                <p className="error">{Object.entries(props.state.error)[0][1]}</p>
+            {showError &&
+                <p className="error">{Object.entries(error)[0][1]}</p>
             }
         </div>
     )
