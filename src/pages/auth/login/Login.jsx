6e288906da8aa868830c11378871e076b8206b88
Toni Rachev <tonirachev92@gmail.com>
2024-06-06 22:05:59 +0300

    Update Login.jsx

--- src/pages/auth/login/Login.jsx
+++ src/pages/auth/login/Login.jsx
@@ -3,13 +3,14 @@ import {useForm} from "../../../hooks/useForm.js";
 import Input from "../../../components/shared/input/Input.jsx";
 
 export default function Login() {
-    const {formData, register} = useForm();
+    const {formData, register, handleSubmit} = useForm();
 
     return (
         <div>
-            <Form>
+            <Form formData={formData} onSubmit={handleSubmit}>
                 <Input label="username" {...register("username", "", {required: true, minLength: 6})}/>
                 <Input label="password" type="password" {...register("password", "", {required: true, minLength: 6})}/>
+                <button type="submit">Login</button>
             </Form>
         </div>
     )
