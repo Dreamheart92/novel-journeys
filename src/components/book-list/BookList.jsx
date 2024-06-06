68f0b1c4b5d179c9bac9c765fad7956bebfb0f8e
Toni Rachev <tonirachev92@gmail.com>
2024-06-06 10:28:23 +0300

    added caption for the book list

--- src/components/book-list/BookList.jsx
+++ src/components/book-list/BookList.jsx
@@ -2,13 +2,16 @@ import BookCard from "../book-card/BookCard.jsx";
 
 import style from "./BookList.module.css";
 
-export default function BookList({books}) {
+export default function BookList({caption, books}) {
 
     return (
         <section className={style.container}>
-            {books.data.products.slice(0, 5).map(book =>
-                <BookCard key={book._id} book={book}/>
-            )}
+            <h3>{caption}</h3>
+            <section className={style['books-list']}>
+                {books.data.products.slice(0, 7).map(book =>
+                    <BookCard key={book._id} book={book}/>
+                )}
+            </section>
         </section>
     )
 }
