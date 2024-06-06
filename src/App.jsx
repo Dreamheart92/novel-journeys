80f80c7d2f257d9dfbad369ff411d05e4cbfd755
Toni Rachev <tonirachev92@gmail.com>
2024-06-06 13:06:31 +0300

    added router outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Header from "./components/header/Header.jsx";
 import Home from "./pages/home/Home.jsx";
+import {Outlet} from "react-router-dom";
 
 export default function App() {
     return (
         <main>
             <Header/>
-            <Home/>
+            <Outlet/>
         </main>
     )
 }
