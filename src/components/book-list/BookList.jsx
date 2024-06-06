2a0ef13d7abdcfcbc6fff0b8fd45e4e47deeb525
Toni Rachev <tonirachev92@gmail.com>
2024-06-06 11:25:30 +0300

    added error checker

--- src/components/book-list/BookList.jsx
+++ src/components/book-list/BookList.jsx
@@ -2,16 +2,23 @@ import BookCard from "../book-card/BookCard.jsx";
 
 import style from "./BookList.module.css";
 
-export default function BookList({caption, books}) {
+export default function BookList({caption, books, error}) {
 
     return (
         <section className={style.container}>
-            <h3>{caption}</h3>
-            <section className={style['books-list']}>
-                {books.data.products.slice(0, 7).map(book =>
-                    <BookCard key={book._id} book={book}/>
-                )}
-            </section>
+            {error && <h3>Failed to fetch data...</h3>}
+
+            {!error &&
+                <>
+                    <h3>{caption}</h3>
+                    <section className={style['books-list']}>
+                        {books.data.products.slice(0, 7).map(book =>
+                            <BookCard key={book._id} book={book}/>
+                        )}
+                    </section>
+                </>
+            }
+
         </section>
     )
 }
