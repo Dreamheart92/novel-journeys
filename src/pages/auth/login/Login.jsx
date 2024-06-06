12a203af7ae90a7c8c02bcdbe75ad2a7ec9ec943
Toni Rachev <tonirachev92@gmail.com>
2024-06-06 14:01:43 +0300

    Update Login.jsx

--- src/pages/auth/login/Login.jsx
+++ src/pages/auth/login/Login.jsx
@@ -1,7 +1,15 @@
+import Form from "../../../components/form/Form.jsx";
+import {useForm} from "../../../hooks/useForm.js";
+import Input from "../../../components/shared/input/Input.jsx";
+
 export default function Login() {
+    const {formData, register} = useForm();
+
     return (
         <div>
-            <h1>Login Page</h1>
+            <Form>
+                <Input {...register("username")}/>
+            </Form>
         </div>
     )
 }
