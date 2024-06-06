b7dd509ab941ec777b26d42c957699e13b614ffc
Toni Rachev <tonirachev92@gmail.com>
2024-06-06 14:01:36 +0300

    register function checking if fieldName exist to prevent infinitive loop + returning onChange func

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,12 +1,27 @@
 import {useState} from "react";
 
-export const useForm = ({initialValue}) => {
+export const useForm = (initialValue = {}) => {
     const [formData, setFormData] = useState(initialValue);
 
     const register = (fieldName, fieldValue = "") => {
-        setFormData(formData =>
-            formData[fieldName] = fieldValue
-        )
+
+        //Check if fieldName exist if would cause infinitive loop
+
+        if (!formData.hasOwnProperty(fieldName)) {
+            setFormData(prevFormState => ({
+                ...prevFormState,
+                [fieldName]: fieldValue
+            }))
+        }
+
+        return {
+            onChange: (event) => {
+                setFormData(prevState => ({
+                    ...prevState,
+                    [fieldName]: event.target.value
+                }))
+            }
+        }
     }
 
     return {
