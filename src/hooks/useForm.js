4ce9e6a342446339b1ca48c31b194036e1e17266
Toni Rachev <tonirachev92@gmail.com>
2024-06-06 13:27:31 +0300

    added register field to the formData

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -2,4 +2,15 @@ import {useState} from "react";
 
 export const useForm = ({initialValue}) => {
     const [formData, setFormData] = useState(initialValue);
+
+    const register = (fieldName, fieldValue = "") => {
+        setFormData(formData =>
+            formData[fieldName] = fieldValue
+        )
+    }
+
+    return {
+        formData,
+        register
+    }
 }
