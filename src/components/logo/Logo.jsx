c30d49f1d9530efd442aabc49f6a6ee94a576194
Toni Rachev <tonirachev92@gmail.com>
2024-06-06 18:47:12 +0300

    added link to the home page

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,7 +1,12 @@
 import style from "./Logo.module.css";
+import {Link} from "react-router-dom";
 
 export default function Logo() {
     return (
-        <h1 className={style.logo}>Logo</h1>
+        <Link to="/">
+            <h1 className={style.logo}>
+                Logo
+            </h1>
+        </Link>
     )
 }
