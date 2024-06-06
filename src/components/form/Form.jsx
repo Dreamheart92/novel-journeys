1c836f0f4c3d041e1e38b652d1dcff3cacf47323
Toni Rachev <tonirachev92@gmail.com>
2024-06-06 14:00:26 +0300

    Update Form.jsx

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,5 +1,7 @@
-export default function Form({children}) {
+export default function Form({formData, children}) {
     return (
-        {children}
+        <>
+            {children}
+        </>
     )
 }
