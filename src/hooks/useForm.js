087dd47e507d611ceb9062f9192995964d690e9f
Toni Rachev <tonirachev92@gmail.com>
2024-06-06 18:47:51 +0300

    added onChange + error field validation

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,9 +1,53 @@
 import {useState} from "react";
+import {normalizeName} from "../utility/utility.js";
+
+const validateField = (value, validations, fieldName) => {
+    const errors = {};
+
+    for (const validationType in validations) {
+        const requirement = validations[validationType];
+
+        switch (validationType) {
+            case "required" : {
+                if (value.trim() === "" && requirement) {
+                    errors[validationType] = `${normalizeName(fieldName)} is required`
+                }
+                break;
+            }
+            case "minLength" : {
+                if (value.trim().length < requirement) {
+                    console.log("min length")
+                    errors[validationType] = `${normalizeName(fieldName)} must be at least ${requirement} characters long`
+                }
+                break;
+            }
+
+            case "maxLength" : {
+                if (value.trim().length > requirement) {
+                    errors[validationType] = `${normalizeName(fieldName)} must not be more than ${requirement} characters`
+                }
+                break;
+            }
+        }
+    }
+
+    return Object.keys(errors).length > 0 ? errors : null;
+}
+
+const checkForFieldErrors = (fieldName, fieldValue, validation, setFieldErrors) => {
+    const fieldErrors = validateField(fieldValue, validation, fieldName);
+
+    setFieldErrors(prevErrors => ({
+        ...prevErrors,
+        [fieldName]: fieldErrors
+    }))
+}
 
 export const useForm = (initialValue = {}) => {
     const [formData, setFormData] = useState(initialValue);
+    const [fieldErrors, setFieldErrors] = useState(null);
 
-    const register = (fieldName, fieldValue = "") => {
+    const register = (fieldName, fieldValue = "", validation = {}) => {
 
         //Check if fieldName exist if would cause infinitive loop
 
@@ -12,14 +56,25 @@ export const useForm = (initialValue = {}) => {
                 ...prevFormState,
                 [fieldName]: fieldValue
             }))
+
+            if (Object.keys(validation).length > 0) {
+                checkForFieldErrors(fieldName, fieldValue, validation, setFieldErrors);
+            }
         }
 
         return {
-            onChange: (event) => {
-                setFormData(prevState => ({
-                    ...prevState,
-                    [fieldName]: event.target.value
-                }))
+            handlers: {
+                onChange: (event) => {
+                    setFormData(prevState => ({
+                        ...prevState,
+                        [fieldName]: event.target.value
+                    }))
+                    checkForFieldErrors(fieldName, event.target.value, validation, setFieldErrors);
+                }
+            },
+            state: {
+                value: formData[fieldName],
+                error: fieldErrors !== null ? fieldErrors[fieldName] || null : null
             }
         }
     }
