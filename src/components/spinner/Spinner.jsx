893ca42b619130dc1ae0013ff106dd31bc818ec0
Toni Rachev <tonirachev92@gmail.com>
2024-06-06 10:38:53 +0300

    added spinner style

--- src/components/spinner/Spinner.jsx
+++ src/components/spinner/Spinner.jsx
@@ -1,3 +1,7 @@
-export default function Spinner() {
+import style from "./Spinner.module.css";
 
+export default function Spinner({...props}) {
+    return (
+        <span {...props} className={style.loader}></span>
+    )
 }
