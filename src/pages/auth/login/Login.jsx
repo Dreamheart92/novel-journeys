7d237fcf80ab94dc5906e84484c2601f9c3040ec
Toni Rachev <tonirachev92@gmail.com>
2024-06-07 11:00:54 +0300

    basic login request

--- src/pages/auth/login/Login.jsx
+++ src/pages/auth/login/Login.jsx
@@ -1,14 +1,27 @@
 import Form from "../../../components/form/Form.jsx";
 import {useForm} from "../../../hooks/useForm.js";
 import Input from "../../../components/shared/input/Input.jsx";
+import {useHttp} from "../../../hooks/useHttp.js";
+import {login} from "../../../api/auth.js";
+import {useEffect} from "react";
 
 export default function Login() {
+    const {data, error, isLoading, sendRequest} = useHttp({initiate: false});
     const {formData, register, handleSubmit} = useForm();
+
+    console.log(data);
+    console.log(error);
+
+    useEffect(() => {
+        if (formData) {
+            sendRequest(login.url, login.settings(formData));
+        }
+    }, [formData]);
 
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <Input label="username" {...register("username", "", {required: true, minLength: 6})}/>
+                <Input label="email" {...register("email", "", {required: true, minLength: 6})}/>
                 <Input label="password" type="password" {...register("password", "", {required: true, minLength: 6})}/>
                 <button type="submit">Login</button>
             </form>
