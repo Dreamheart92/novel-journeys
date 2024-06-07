edfa81249a7c9e06092a2eb6067de0083c60139b
Toni Rachev <tonirachev92@gmail.com>
2024-06-07 12:33:17 +0300

    switching between login and register depends on path + heading and caption

--- src/pages/auth/auth/Auth.jsx
+++ src/pages/auth/auth/Auth.jsx
@@ -1,7 +1,30 @@
-export default function Auth() {
-    return (
-        <section>
+import {useLocation} from "react-router-dom";
 
+import Login from "../login/Login.jsx";
+import Signup from "../signup/Signup.jsx";
+
+import style from "./Auth.module.css";
+
+export default function Auth() {
+    const path = useLocation().pathname;
+
+    const isOnLoginPage = path === "/login";
+    const heading = isOnLoginPage ? "Login" : "Signup";
+    const caption = isOnLoginPage ?
+        "Welcome Back to Novel Journeys – Dive Back Into Your Story!"
+        : "Join Novel Journeys Today – Your Next Adventure Awaits!"
+
+    return (
+        <section className={style.container}>
+            <div className={style.heading}>
+                <h2>{heading}</h2>
+                <p>{caption}</p>
+            </div>
+
+            <div className={style['auth-container']}>
+                {isOnLoginPage && <Login formStyle={style.form}/>}
+                {!isOnLoginPage && <Signup/>}
+            </div>
         </section>
     )
 }
