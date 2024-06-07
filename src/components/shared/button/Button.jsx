bba3a8e1f08591a64be3970d15a6777bcbb79804
Toni Rachev <tonirachev92@gmail.com>
2024-06-07 13:18:22 +0300

    added classname and spreading props

--- src/components/shared/button/Button.jsx
+++ src/components/shared/button/Button.jsx
@@ -1,5 +1,9 @@
 import style from "./Button.module.css";
 
-export default function Button() {
+export default function Button({className, children, ...props}) {
+    const buttonStyle = style[className] || style.primary;
 
+    return (
+        <button className={`${buttonStyle} ${style.btn}`} {...props}>{children}</button>
+    )
 }
