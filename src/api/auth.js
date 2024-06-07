13b0b72479fcffb5daca5c4007cd8d8f09196527
Toni Rachev <tonirachev92@gmail.com>
2024-06-07 11:00:06 +0300

    added settings for the login request

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,3 +1,20 @@
 import {API_ENDPOINTS} from "../constants/api.endpoints.js";
 
-export const login = API_ENDPOINTS.auth.login;
+export const login = {
+    url: API_ENDPOINTS.auth.login,
+    settings: (userData) => {
+
+        const body = {
+            email: userData.email.value,
+            password: userData.password.value
+        }
+
+        return {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(body)
+        }
+    }
+};
