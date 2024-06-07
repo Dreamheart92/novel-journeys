bfd35295013f4643e8a8445e1b52f9d4a95b29bf
Toni Rachev <tonirachev92@gmail.com>
2024-06-08 00:47:08 +0300

    added reducers storeUser and deleteUser

--- src/store/user.slice.js
+++ src/store/user.slice.js
@@ -5,5 +5,15 @@ const initialState = {user: null};
 const userSlice = createSlice({
     name: "user",
     initialState,
-    reducers: {}
+    reducers: {
+        storeUser(state, action) {
+            state.user = action.payload.user;
+        },
+        deleteUser(state, action) {
+            state.user = null;
+        }
+    }
 })
+
+export const userActions = userSlice.actions;
+export default userSlice.reducer;
