9e14705165c73f12838b0bc5eb663660aeaa82ed
Toni Rachev <tonirachev92@gmail.com>
2024-06-08 00:45:28 +0300

    added usermenu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,16 @@
 import Search from "../search/Search.jsx";
-import AccountButton from "../account-button/AccountButton.jsx";
+import UserMenu from "../user-menu/UserMenu.jsx";
 import Menu from "../menu/Menu.jsx";
 
 import style from "./Header.module.css";
 import MenuItem from "../menu-item/MenuItem.jsx";
 import Logo from "../logo/Logo.jsx";
+import {removeUserDataFromLocalStorage} from "../../utility/storage.js";
+import {useSelector} from "react-redux";
 
 export default function Header() {
+    const {user} = useSelector(state => state.user);
+
     return (
         <header className={style.header}>
             <div className={style['header-primary']}>
@@ -17,7 +21,7 @@ export default function Header() {
                 </div>
 
                 <div>
-                    <AccountButton/>
+                    <UserMenu isLoggedIn={user}/>
                 </div>
 
             </div>
