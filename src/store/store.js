6e5de8d5131d3c91ba295aa13d49f4aec82126f0
Toni Rachev <tonirachev92@gmail.com>
2024-06-08 00:46:54 +0300

    created a store

--- src/store/store.js
+++ src/store/store.js
@@ -1,2 +1,8 @@
 import {configureStore} from "@reduxjs/toolkit";
+import userSlice from "./user.slice.js";
 
+const store = configureStore({
+    reducer: {user: userSlice}
+})
+
+export default store;
