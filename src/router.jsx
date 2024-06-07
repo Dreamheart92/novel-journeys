17f26be343804d8304af872db0f030cf13c46d98
Toni Rachev <tonirachev92@gmail.com>
2024-06-08 00:46:49 +0300

    added paths

--- src/router.jsx
+++ src/router.jsx
@@ -3,21 +3,22 @@ import {createBrowserRouter} from "react-router-dom";
 import App from "./App.jsx";
 import Home from "./pages/home/Home.jsx";
 import Auth from "./pages/auth/auth/Auth.jsx";
+import {PATHS} from "./constants/paths.js";
 
 export const router = createBrowserRouter([
     {
         element: <App/>,
         children: [
             {
-                path: "/",
+                path: PATHS.home,
                 element: <Home/>
             },
             {
-                path: "/login",
+                path: PATHS.login,
                 element: <Auth/>
             },
             {
-                path: "/signup",
+                path: PATHS.signup,
                 element: <Auth/>
             }
         ]
