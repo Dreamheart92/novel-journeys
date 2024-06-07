60118bb60641fc2b6172d78efc823a8f2c704afc
Toni Rachev <tonirachev92@gmail.com>
2024-06-07 21:20:41 +0300

    label is rendering children caption

--- src/components/shared/input/Input.jsx
+++ src/components/shared/input/Input.jsx
@@ -1,14 +1,14 @@
 import style from "./Input.module.css";
 
-export default function Input({label, type = "text", ...props}) {
+export default function Input({label, type = "text", children, ...props}) {
     const value = props.state.value;
     const error = props.state.error;
 
-    const showError = (error && props.state.isDirty) || (error && props.formState.isSubmittedAndHasErrors);
+    const showError = error && (props.state.isDirty || props.formState.isSubmittedAndHasErrors);
 
     return (
         <div className={style.container}>
-            <label htmlFor={label}>{label[0].toUpperCase() + label.slice(1)}</label>
+            <label htmlFor={label}>{children}</label>
             <input className={style['text-field']} {...props.handlers} type={type} name={label} value={value}/>
 
             {showError &&
