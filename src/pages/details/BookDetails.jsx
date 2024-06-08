14fd74009efe7f80a650a3919c4c88c97cded2cb
Toni Rachev <tonirachev92@gmail.com>
2024-06-08 09:22:56 +0300

    fetching book by id

--- src/pages/details/BookDetails.jsx
+++ src/pages/details/BookDetails.jsx
@@ -1,4 +1,11 @@
+import {useParams} from "react-router-dom";
+import {useHttp} from "../../hooks/useHttp.js";
+import {getBookById} from "../../api/books.js";
+
 export default function BookDetails() {
+    const {id: bookId} = useParams();
+    const {data: bookData, isLoading, error} = useHttp({url: getBookById(bookId)});
+
     return (
         <section>
             Book details
