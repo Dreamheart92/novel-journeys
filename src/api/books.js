a7c180d6a5677d1f582146b74ef823abb821398e
Toni Rachev <tonirachev92@gmail.com>
2024-06-08 09:22:20 +0300

    added get book by id

--- src/api/books.js
+++ src/api/books.js
@@ -1,3 +1,7 @@
 import {API_ENDPOINTS} from "../constants/api.endpoints.js";
 
 export const getRecentBooks = API_ENDPOINTS.books.getBooks + "?sortBy=newest";
+
+export const getBookById = (bookId) => {
+    return API_ENDPOINTS.books.getBooks + "/" + bookId
+}
