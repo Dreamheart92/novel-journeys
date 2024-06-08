ee7ac4f9ea68a49bcfcc429a52585eabaff805c3
Toni Rachev <tonirachev92@gmail.com>
2024-06-08 17:42:07 +0300

    created book details template

--- src/pages/details/BookDetails.jsx
+++ src/pages/details/BookDetails.jsx
@@ -2,13 +2,92 @@ import {useParams} from "react-router-dom";
 import {useHttp} from "../../hooks/useHttp.js";
 import {getBookById} from "../../api/books.js";
 
+import style from "./BookDetails.module.css";
+import Button from "../../components/shared/button/Button.jsx";
+import Rating from "../../components/rating/Rating.jsx";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faHeart} from "@fortawesome/free-solid-svg-icons";
+import {convertDate} from "../../utility/convertDate.js";
+
 export default function BookDetails() {
     const {id: bookId} = useParams();
     const {data: bookData, isLoading, error} = useHttp({url: getBookById(bookId)});
 
     return (
-        <section>
-            Book details
+        <section className={style.container}>
+            <div className={style.cover}>
+                <img src={bookData?.imageUrl} alt=""/>
+            </div>
+
+            <div className={style.info}>
+                <div className={style.test}>
+
+                    <div className={style.heading}>
+                        <h2>{bookData?.name}</h2>
+                        <p>{bookData?.author}</p>
+                    </div>
+
+                    <div className={style.publisher}>
+                        <h3>Publisher</h3>
+                        <p>{bookData?.publisher}</p>
+                    </div>
+
+                    <div className={style.price}>
+                        <span>{bookData?.price}</span>
+                    </div>
+                </div>
+
+                <div className={style.data}>
+                    <div className={style.wrapper}>
+                        <p>Pages: </p>
+                        <p>{bookData?.pages}</p>
+                    </div>
+
+                    <div className={style.wrapper}>
+                        <p>Publication date: </p>
+                        <p>{convertDate(bookData?.publicationDate)}</p>
+                    </div>
+
+                    <div className={style.wrapper}>
+                        <p>Language: </p>
+                        <p>{bookData?.language}</p>
+                    </div>
+                </div>
+
+                <div className={style.genre}>
+                    <div className={style.tag}>
+                        <Button className={"tag"}>{bookData?.genre}</Button>
+                    </div>
+                </div>
+
+                <div className={style.rating}>
+                    <Rating/>
+                    <p>/ 10 ratings</p>
+                </div>
+
+                <hr className={style.border}/>
+
+                <div className={style.description}>
+                    <h3>Description</h3>
+                    <p>{bookData?.description}</p>
+                </div>
+
+                <div className={style.controllers}>
+                    <div style={{width: "10em"}}>
+                        <Button>
+                            Add to cart
+                        </Button>
+                    </div>
+
+                    <div style={{width: "10em"}}>
+                        <Button className={"secondary"}>
+                            Add to wishlist
+                        </Button>
+                    </div>
+                </div>
+
+
+            </div>
         </section>
     )
 }
