c7883d8f630019432784146c0d93cf65b20979dd
Toni Rachev <tonirachev92@gmail.com>
2024-06-08 09:22:48 +0300

    added link to redirect a book to details page

--- src/components/book-card/BookCard.jsx
+++ src/components/book-card/BookCard.jsx
@@ -1,9 +1,12 @@
 import style from "./BookCard.module.css";
+import {Link, useNavigate} from "react-router-dom";
 
 export default function BookCard(book) {
     return (
         <div className={style.card}>
-            <img src={book.book.imageUrl} alt=""/>
+            <Link to={`/book/${book.book._id}`}>
+                <img src={book.book.imageUrl} alt=""/>
+            </Link>
         </div>
     )
 }
