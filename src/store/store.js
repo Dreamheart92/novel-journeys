9585cd693cb3c0e84ae6e803a42b0043068b943d
Toni Rachev <tonirachev92@gmail.com>
2024-06-09 16:09:50 +0300

    added cartSlice

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,9 @@
 import {configureStore} from "@reduxjs/toolkit";
 import userSlice from "./user.slice.js";
+import cartSlice from "./cart.slice.js";
 
 const store = configureStore({
-    reducer: {user: userSlice}
+    reducer: {user: userSlice, cart: cartSlice}
 })
 
 export default store;
