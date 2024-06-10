2f05f013772e42d6d191b15e9e32bdd8affc8a02
Toni Rachev <tonirachev92@gmail.com>
2024-06-10 11:41:08 +0300

    added cart endpoints

--- src/constants/api.endpoints.js
+++ src/constants/api.endpoints.js
@@ -1,7 +1,7 @@
 const baseUrl = 'http://localhost:3000/api/v1';
-
 const books = baseUrl + "/products";
 const auth = baseUrl + "/auth";
+const cart = baseUrl + "/cart";
 
 export const API_ENDPOINTS = {
     books: {
@@ -10,5 +10,10 @@ export const API_ENDPOINTS = {
     auth: {
         login: auth + "/login",
         signup: auth + "/signup"
+    },
+    cart: {
+        addItemToCart: cart,
+        removeItemFromCart: cart + "/remove",
+        deleteItemFromCart: cart + "/delete"
     }
 }
