029c58b2013dd0abb2f3e82ecc59260ba97487c4
Toni Rachev <tonirachev92@gmail.com>
2024-06-10 11:42:19 +0300

    Update BookDetails.jsx

--- src/pages/details/BookDetails.jsx
+++ src/pages/details/BookDetails.jsx
@@ -2,20 +2,22 @@ import {useParams} from "react-router-dom";
 import {useHttp} from "../../hooks/useHttp.js";
 import {getBookById} from "../../api/books.js";
 
-import style from "./BookDetails.module.css";
 import Button from "../../components/shared/button/Button.jsx";
 import Rating from "../../components/rating/Rating.jsx";
 import {convertDate} from "../../utility/convertDate.js";
-import {useDispatch} from "react-redux";
-import {cartActions} from "../../store/cart.slice.js";
+import {useDispatch, useSelector} from "react-redux";
+import {cartActions, updateCart} from "../../store/cart.slice.js";
+
+import style from "./BookDetails.module.css";
 
 export default function BookDetails() {
     const dispatch = useDispatch();
     const {id: bookId} = useParams();
     const {data: bookData, isLoading, error} = useHttp({url: getBookById(bookId)});
+    const {user} = useSelector(state => state.user);
 
     const handleAddItemToCart = (book) => {
-        dispatch(cartActions.addItemToCart({book}));
+        dispatch(updateCart({book, accessToken: user.accessToken, actionType: "add"}));
         dispatch(cartActions.openCart());
     }
 
