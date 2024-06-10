ce14cb6faa0df63bce65fe3ce374cc23b7846772
Toni Rachev <tonirachev92@gmail.com>
2024-06-10 11:42:46 +0300

    added updateCart async chunk for making request to database and storing the products to local cart

--- src/store/cart.slice.js
+++ src/store/cart.slice.js
@@ -1,32 +1,16 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import {calculateCartQuantityAndCost} from "../utility/calculateCartQuantityAndCost.js";
+import {sendHttpRequest} from "../hooks/useHttp.js";
+import {addItemToCartSettings, removeItemFromCartSettings} from "../api/cart.js";
 
-const initialState = {cart: [], isCartOpen: false};
+const initialState = {cart: [], isCartOpen: false, total: {price: 0, quantity: 0}};
 
 const cartSlice = createSlice({
     name: "cart",
     initialState,
     reducers: {
-        getCart(state, action) {
-            return state.cart;
-        },
-        addItemToCart(state, action) {
-            const bookToAdd = action.payload.book;
-            const isBookAlreadyInCart = state.cart.find(item => item.book._id === bookToAdd._id);
-
-            if (isBookAlreadyInCart) {
-                isBookAlreadyInCart.quantity++;
-            } else {
-                state.cart.push({book: bookToAdd, quantity: 1});
-            }
-        },
         removeItemFromCart(state, action) {
-            const book = state.cart.find(item => item.book._id === action.payload.bookId);
 
-            book.quantity -= 1;
-
-            if (book.quantity <= 0) {
-                state.cart.splice(book, 1);
-            }
         },
         openCart(state, action) {
             state.isCartOpen = true;
@@ -34,8 +18,72 @@ const cartSlice = createSlice({
         closeCart(state, action) {
             state.isCartOpen = false;
         }
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(updateCart.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(updateCart.fulfilled, (state, action) => {
+                const {actionType, book} = action.payload;
+
+                switch (actionType) {
+                    case "add" : {
+                        const isBookAlreadyInCart = state.cart.find(item => item.book._id === book._id);
+
+                        if (isBookAlreadyInCart) {
+                            isBookAlreadyInCart.quantity++;
+                        } else {
+                            state.cart.push({book, quantity: 1});
+                        }
+                        break;
+                    }
+
+                    case "remove" : {
+                        const bookItem = state.cart.find(item => item.book._id === book._id);
+
+                        bookItem.quantity -= 1;
+
+                        if (bookItem.quantity <= 0) {
+                            state.cart.splice(bookItem, 1);
+                        }
+                        break;
+                    }
+                }
+
+
+                state.total = calculateCartQuantityAndCost(state.cart);
+                state.isLoading = false;
+            })
+            .addCase(updateCart.rejected, (state, action) => {
+                console.log("rejected");
+                state.isLoading = false;
+            })
     }
 })
+
+export const updateCart = createAsyncThunk(
+    "cart/updateCart",
+    async (data, {dispatch}) => {
+        const {actionType, book, accessToken} = data;
+
+        const requestSettings = actionType === "add"
+            ? addItemToCartSettings(book._id, accessToken)
+            : removeItemFromCartSettings(book._id, accessToken);
+
+        try {
+            const response = await sendHttpRequest(requestSettings.url, requestSettings.settings);
+
+            if (!response.success) {
+                throw response;
+            }
+
+            return {book: data.book, actionType};
+        } catch (error) {
+            throw error;
+        }
+    }
+)
 
 export const cartActions = cartSlice.actions;
 export default cartSlice.reducer;
