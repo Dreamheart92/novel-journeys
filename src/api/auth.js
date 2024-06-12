165d89bab2f769681af623b56298e3f3262735e4
Toni Rachev <tonirachev92@gmail.com>
2024-06-12 10:32:28 +0300

    added createGuest request

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,4 +1,6 @@
 import {API_ENDPOINTS} from "../constants/api.endpoints.js";
+import {sendHttpRequest} from "../hooks/useHttp.js";
+import {storeGuestToLocalStorage} from "../utility/storage.js";
 
 export const login = {
     url: API_ENDPOINTS.auth.login,
@@ -37,4 +39,16 @@ export const signup = {
             body: JSON.stringify(body)
         }
     }
+}
+
+export const registerGuestSettings = {
+    url: API_ENDPOINTS.auth.guest,
+    settings: {
+        method: "Post"
+    }
+}
+
+export const createGuest = async () => {
+    sendHttpRequest(registerGuestSettings.url, registerGuestSettings.settings)
+        .then((guestData) => storeGuestToLocalStorage(guestData.data));
 }
